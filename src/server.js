ae5b67fcbf013b660d9097fa144a2a1d81925c0e
spooky <spooky@pop-os.localdomain>
2022-11-06 20:56:33 -0500

    re-add module alias

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('module-alias/register');
 const { Client, GatewayIntentBits } = require('discord.js');
 const { handleInteractions } = require('./commands');
 const { getConnectionPool, keepPoolAlive } = require('@utility/postgres');
